9ca661e7bc71f6fb4fe577dcec8680d6613a1b67
GonzaloJVicenteVeron <gonzalojvicente01@gmail.com>
2023-07-10 19:44:39 -0300

    Exercise 1

---
+++ scripts.js
@@ -0,0 +1,13 @@
+//Ejercicio 1 
+
+function ParOImpar (numero) {
+    if (numero % 2 === 0) {
+        console.log (numero + " es un número par")
+    }
+        else {
+            console.log (numero + " es un numero impar")
+        }
+
+    }
+
+    
