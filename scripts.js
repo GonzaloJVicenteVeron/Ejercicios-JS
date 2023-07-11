375f9a4ed50892a6d6c58b76d65bdffc2ed1b49d
GonzaloJVicenteVeron <gonzalojvicente01@gmail.com>
2023-07-10 22:20:43 -0300

    Exercise 2

--- scripts.js
+++ scripts.js
@@ -2,12 +2,25 @@
 
 function ParOImpar (numero) {
     if (numero % 2 === 0) {
-        console.log (numero + " es un número par")
+        console.log (numero + " es un número par");
     }
         else {
-            console.log (numero + " es un numero impar")
+            console.log (numero + " es un numero impar");
         }
 
     }
 
-    
+// Ejercicio 2
+
+function numeromayor (num1, num2) {
+    if (num1 > num2) {
+        console.log (num1 + " es mayor que " + num2);
+    }
+    else if (num1 < num2) {
+        console.log (num2 + " es mayor que " + num1);
+    }
+    else {
+        console.log ("los numeros son iguales");
+    }
+
+}
